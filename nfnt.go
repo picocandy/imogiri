9b23460dc6bd74ce80c71025065fb71d2d80caf8
Alif Rachmawadi <subosito@gmail.com>
2015-01-24 21:36:03 +0700

    small refactoring

--- nfnt.go
+++ nfnt.go
@@ -16,22 +16,31 @@ func (n NFNT) Resize(w io.Writer, r io.Reader, opt ResizeOption) error {
 		return err
 	}
 
-	var img image.Image
-
-	switch opt.Format {
-	case "jpg":
-		img, err = jpeg.Decode(r)
-	case "png":
-		img, err = png.Decode(r)
-	}
-
+	img, err := n.decode(r, opt.Format)
 	if err != nil {
 		return err
 	}
 
 	m := resize.Resize(opt.Width, opt.Height, img, resize.Bicubic)
 
-	switch opt.Format {
+	return n.encode(w, m, opt.Format)
+}
+
+func (n NFNT) decode(r io.Reader, format string) (m image.Image, err error) {
+	switch format {
+	case "jpg":
+		m, err = jpeg.Decode(r)
+	case "png":
+		m, err = png.Decode(r)
+	}
+
+	return
+}
+
+func (n NFNT) encode(w io.Writer, m image.Image, format string) error {
+	var err error
+
+	switch format {
 	case "jpg":
 		err = jpeg.Encode(w, m, &jpeg.Options{})
 	case "png":
