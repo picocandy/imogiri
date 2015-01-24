44dcee6267be1da96740bb2529ac76328c3e0e7e
Alif Rachmawadi <subosito@gmail.com>
2015-01-24 19:57:34 +0700

    more tests

--- imogiri_test.go
+++ imogiri_test.go
@@ -2,6 +2,7 @@ package imogiri
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"github.com/stretchr/testify/assert"
 	"io"
@@ -11,18 +12,45 @@ import (
 )
 
 type ResizerTest struct{}
+type ResizerTestFailure struct{}
 
 func (z *ResizerTest) Resize(w io.Writer, r io.Reader, opt ResizeOption) error {
 	return nil
 }
 
+func (z *ResizerTestFailure) Resize(w io.Writer, r io.Reader, opt ResizeOption) error {
+	return errors.New("Unable to resize the image")
+}
+
+func TestImogiri_Resize_noEngine(t *testing.T) {
+	g := &Imogiri{ResizerEngines: []Resizer{}}
+	s := new(bytes.Buffer)
+	r := new(bytes.Reader)
+	x := ResizeOption{}
+
+	err := g.Resize(s, r, x)
+	assert.NotNil(t, err)
+	assert.Equal(t, "No registered engine for resizing image", err.Error())
+}
+
+func TestImogiri_Resize_failure(t *testing.T) {
+	g := &Imogiri{ResizerEngines: []Resizer{&ResizerTestFailure{}}}
+	s := new(bytes.Buffer)
+	r := new(bytes.Reader)
+	x := ResizeOption{}
+
+	err := g.Resize(s, r, x)
+	assert.NotNil(t, err)
+	assert.Equal(t, "Unable to resize the image", err.Error())
+}
+
 func TestImogiri_Resize(t *testing.T) {
 	g := &Imogiri{ResizerEngines: []Resizer{&ResizerTest{}}}
-	b := bytes.NewBuffer([]byte{})
+	s := new(bytes.Buffer)
 	r := loadFixture("gopher.png")
 	x := ResizeOption{Width: 80, Height: 80, Format: "png"}
 
-	err := g.Resize(b, r, x)
+	err := g.Resize(s, r, x)
 	assert.Nil(t, err)
 }
 
